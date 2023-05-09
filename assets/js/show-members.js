f66635278a6f77fbc1c8afeacc62c2874c9249f7
AliHMohammad <alihmohammad@hotmail.com>
2023-05-09 12:59:16 +0200

    Initial saves

--- assets/js/show-members.js
+++ assets/js/show-members.js
@@ -11,11 +11,14 @@ function showMembers(members) {
 function showMember(member) {
     const htmlGrid = /*html*/`
     <article class="grid-item">
-        
+        <img src=${member.image}>
+        <p>${member.firstName} ${member.lastName}</p>
     </article>
     `
 
-     
+    document.querySelector("#members-grid").insertAdjacentHTML("beforeend", htmlGrid);
+
+    
 }
 
 function clearContent() {
