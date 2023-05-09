a8e7fbd6aa5975807398be2b15ff465abb00e70a
YawHB <yawhboateng@gmail.com>
2023-05-09 20:57:35 +0200

    Deleted file

--- assets/js/delete-member.js
+++
@@ -1,6 +0,0 @@
-/*
-Hey Chris - jeg har nogle få spørgsmål angående min delete user story
-1. Er det korrekt at jeg har lavet en nyt modul til delete eller skulle den ligge i api.js?
-2. Er det korrekt forstået at jeg skal kalde min function apiDeleteMember?
-3. skal jeg importerer eller eksportere apiDeleteMember?
-*/
