a2a58ff8f4c36d6b65f774f428ec43088442be60
Forkeh <forkeh@gmail.com>
2023-05-09 19:36:07 +0200

    filter is working on 4 properties

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,10 +1,28 @@
+import { members } from "./api.js";
+import { showMembers } from "./show-members.js";
 
 function filterChange(event) {
-    const filter = document.querySelector('#filter').value;
-    console.log(filter);
+	const filter = document.querySelector("#filter").value;
+	console.log("current filter: " + filter);
 
-    
-    
+	if (filter === "all") {
+		showMembers(members);
+	} else {
+		const filteredMembers = members.filter(filterProperty);
+		showMembers(filteredMembers);
+
+		function filterProperty(member) {
+			if (filter === "active") {
+				return member["isActiveMember"] === true;
+			} else if (filter === "inactive") {
+				return member["isActiveMember"] === false;
+			} else if (filter === "competitive") {
+				return member["isCompetitive"] === true;
+			} else if (filter === "casual") {
+				return member["isCompetitive"] === false;
+			}
+		}
+	}
 }
 
-export { filterChange }
+export { filterChange };
