08e78b51755119f5ae74a3712e843a77eb2bc52d
YawHB <yawhboateng@gmail.com>
2023-05-09 20:18:51 +0200

    Comments  to Chris and import

--- assets/js/delete-member.js
+++ assets/js/delete-member.js
@@ -1,3 +1,12 @@
+/*
+Hey Chris - jeg har nogle få spørgsmål angående min delete user story
+1. Er det korrekt at jeg har lavet en nyt modul til delete eller skulle den ligge i api.js?
+2. Er det korrekt forstået at jeg skal kalde min function apiDeleteMember?
+3. skal jeg importerer eller eksportere apiDeleteMember?
+*/
+
+import { endpoint } from "./api.js";
+
 async function apiDeleteMember(member) {
 	const response = await fetch(`${endpoint}/members/${member.id}.json`, { method: "DELETE" });
 	if (response.ok) {
