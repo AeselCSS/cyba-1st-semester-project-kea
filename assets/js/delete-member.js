ec88cb6bbf494fd8b9eae527635ec1bf3434f558
YawHB <yawhboateng@gmail.com>
2023-05-09 20:55:38 +0200

    Moved Delete function to api.js

--- assets/js/delete-member.js
+++ assets/js/delete-member.js
@@ -4,12 +4,3 @@ Hey Chris - jeg har nogle få spørgsmål angående min delete user story
 2. Er det korrekt forstået at jeg skal kalde min function apiDeleteMember?
 3. skal jeg importerer eller eksportere apiDeleteMember?
 */
-
-import { endpoint } from "./api.js";
-
-async function apiDeleteMember(member) {
-	const response = await fetch(`${endpoint}/members/${member.id}.json`, { method: "DELETE" });
-	if (response.ok) {
-		console.log("Member successfully deleted");
-	}
-}
