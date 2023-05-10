4c3e1223a6ab958371f04fe95de3e95a63e20ad9
Forkeh <forkeh@gmail.com>
2023-05-10 10:19:17 +0200

    search and filter works in tandem, can optimize code a bit

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,11 +1,20 @@
 import { members } from "./api.js";
 import { showMembers } from "./show-members.js";
+import { searchMemberProperties } from "./search.js";
 
 function filterChange() {
+	// Get current selected filter and search value
 	const filter = document.querySelector("#filter").value;
+	const searchValue = document.querySelector("#search").value.toLowerCase();
+
+	// Create filtered members list
 	const filteredMembers = filterMembers(filter);
-	showMembers(filteredMembers);
-	
+
+	// Create list by searching through filtered list members properties
+	const searchedMembers = searchMemberProperties(filteredMembers, searchValue);
+
+	// Update view
+	showMembers(searchedMembers);
 }
 
 function filterMembers(filter) {
--- assets/js/search.js
+++ assets/js/search.js
@@ -2,42 +2,47 @@ import { showMembers } from "./show-members.js";
 import { filterMembers } from "./filter.js";
 
 function searchbar() {
-	const searchValue = document.querySelector("#search").value.toLowerCase();
+	// Get current selected filter and search value
 	const filter = document.querySelector("#filter").value;
+	const searchValue = document.querySelector("#search").value.toLowerCase();
 
-	const filteredArray = filterMembers(filter);
+	// Create filtered members list
+	const filteredMembers = filterMembers(filter);
 
-	const searchArray = checkAllProperties(filteredArray);
+	// Create list by searching through filtered list members properties
+	const searchedMembers = searchMemberProperties(filteredMembers, searchValue);
 
-	showMembers(searchArray);
+	// Update view
+	showMembers(searchedMembers);
+}
 
-	function checkAllProperties(array) {
-		const searchArray = [];
+function searchMemberProperties(array, searchValue) {
+	const searchMembers = [];
 
-		//TODO: Combine with filter
+	//TODO: Combine with filter
 
-		// Loops through every member
-		for (const member of array) {
-			// Loops through every key in current member
-			for (const key in member) {
-				const value = member[key];
-				//Checks strings for a match
-				if (typeof value === "string") {
-					if (checkString(value)) {
-						searchArray.push(member);
-						break;
-					}
-					// Checks arrays for a match
-				} else if (typeof value === "object") {
-					if (checkArray(value)) {
-						searchArray.push(member);
-						break;
-					}
+	// Loops through every member
+	for (const member of array) {
+		// Loops through every key in current member
+		for (const key in member) {
+			const value = member[key];
+			//Checks strings for a match
+			if (typeof value === "string") {
+				if (checkString(value)) {
+					searchMembers.push(member);
+					break;
+				}
+				// Checks arrays for a match
+			} else if (typeof value === "object") {
+				if (checkArray(value)) {
+					searchMembers.push(member);
+					break;
 				}
 			}
 		}
-		return searchArray;
 	}
+
+	return searchMembers;
 
 	function checkString(string) {
 		return string.toLowerCase().includes(searchValue);
@@ -49,4 +54,4 @@ function searchbar() {
 	}
 }
 
-export { searchbar };
+export { searchbar, searchMemberProperties };
