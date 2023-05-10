07ed8209984f369d404dcdf9801431d467d5c057
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-05-10 10:11:55 +0200

    fixed an issue with disciplines values

--- assets/js/create-member.js
+++ assets/js/create-member.js
@@ -5,6 +5,9 @@ const defaultAvatar = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-prof
 
 // createMember form upon clicking the create new member button
 function createMemberForm() {
+	// clear the dialog content if any - we need to do this smarter and uniform in the future
+    document.querySelector("#main-dialog").innerHTML = "";
+	// create the form
 	const createMemberForm = /*html*/ `
     <form id="create-member-form">
         <label for="firstName">First name</label>
@@ -17,7 +20,7 @@ function createMemberForm() {
         <input type="email" name="email" id="email" required>
         
         <label for="image">Image</label>
-        <input type="url" name="image" id="image" placeholder=${defaultAvatar} required>
+        <input type="url" name="image" id="image" value=${defaultAvatar} required>
         
         <label for="dateOfBirth">Date of birth</label>
         <input type="date" name="dateOfBirth" id="dateOfBirth" required>
@@ -56,19 +59,19 @@ function createMemberForm() {
         `;
 	document.querySelector("#main-dialog").insertAdjacentHTML("beforeend", createMemberForm);
 	const form = document.querySelector("#create-member-form");
-    
+
 	// Show/hide the disciplines container based on the member type
 	const memberTypeSelect = form.querySelector("#memberType");
 	const disciplinesContainer = form.querySelector("#disciplines-container");
 	memberTypeSelect.addEventListener("change", () => {
-        if (memberTypeSelect.value === "competitive") {
-            disciplinesContainer.style.display = "block";
+		if (memberTypeSelect.value === "competitive") {
+			disciplinesContainer.style.display = "block";
 		} else {
-            disciplinesContainer.style.display = "none";
+			disciplinesContainer.style.display = "none";
 		}
 	});
-    
-    // add event listener to submit and reset buttons
+
+	// add event listener to submit and reset buttons
 	form.addEventListener("submit", createMember);
 	form.addEventListener("reset", () => form.reset());
 
@@ -77,33 +80,47 @@ function createMemberForm() {
 
 // createMember function
 async function createMember(event) {
-    event.preventDefault();
-    const form = event.target;
+	event.preventDefault();
+
+    const dialog = document.querySelector("#main-dialog");
+	const form = event.target;
+	
     const newMember = {
-        firstName: form.firstName.value,
-        lastName: form.lastName.value,
-        email: form.email.value,
-        image: form.image.value,
-        dateOfBirth: form.dateOfBirth.value,
-        gender: form.gender.value,
-        isActiveMember: form.membershipStatus.value === "active",
-        isCompetitive: form.memberType.value === "competitive",
-        disciplines: form.disciplines.value,
-    }
-    // call apiCreateMember function from api.js
-    const response = await apiCreateMember(member);
-    if (response) {
-        form.reset();
-        document.querySelector("#main-dialog").close();
-        // TODO: show success message to user
-        // TODO: update members list
-    } else {
-        console.log("Error creating member");
-        // TODO: show error message to user
-    }
+		firstName: form.firstName.value,
+		lastName: form.lastName.value,
+		email: form.email.value,
+		image: form.image.value,
+		dateOfBirth: form.dateOfBirth.value,
+		gender: form.gender.value,
+		isActiveMember: form.membershipStatus.value === "active",
+		isCompetitive: form.memberType.value === "competitive",
+		// add disciplines as an array of selescted values, but only if member type is competitive
+		// [...form.disciplines] is a spread operator that converts a NodeList to an array
+		// discipline.checked is a boolean value that is true if the checkbox is checked
+		// the .map method is used to retrieve the "value" property of each selected checkbox, and returns them as a new array
+		// the array is then assigned to the disciplines property of the newMember object
+		// if the member type is not "competitive", the disciplines property is set to an empty array
+		disciplines:
+			form.memberType.value === "competitive"
+				? [...form.disciplines].filter((discipline) => discipline.checked).map((discipline) => discipline.value)
+				: [],
+	};
+
+	// call apiCreateMember function from api.js
+	const response = await apiCreateMember(newMember);
+	if (response.ok) {
+		console.log("Member created");
+        console.log(newMember);
+		// reset the form, close the dialog and clear the dialog content
+		form.reset();
+		dialog.close();
+		dialog.innerHTML = "";
+		// TODO: show success message to user
+		// TODO: update members list
+	} else {
+		console.log("Error occured while creating new member");
+		// TODO: show error message to user
+	}
 }
 
 export { createMemberForm };
-
-
-
