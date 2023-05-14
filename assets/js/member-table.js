85c8a538f5ab48543b7bf3c68d43e0ac58dd47fe
YawHB <yawhboateng@gmail.com>
2023-05-14 14:54:20 +0200

    Added function that populates number of active members in all financial tables

--- assets/js/member-table.js
+++ assets/js/member-table.js
@@ -2,27 +2,34 @@ import { members } from "./api.js";
 
 function calculateMembersCount() {
 	const activeMembersSum = countActiveMembers();
-    const inactiveMembersSum = countInactiveMembers();
-    const totalMembersCount = activeMembersSum + inactiveMembersSum;
+	const inactiveMembersSum = countInactiveMembers();
+	const totalMembersCount = activeMembersSum + inactiveMembersSum;
 
-    populateMembersTable(activeMembersSum, inactiveMembersSum, totalMembersCount)
+	populateMembersTable(activeMembersSum, inactiveMembersSum, totalMembersCount);
 }
 
 function countActiveMembers() {
-	const activeMembersArr = members.filter(member => member.isActiveMember === true)
-	return activeMembersArr.length
+	const activeMembersArr = members.filter((member) => member.isActiveMember === true);
+	return activeMembersArr.length;
 }
 
 function countInactiveMembers() {
-    const inactiveMembersArr = members.filter((member) => member.isActiveMember === false);
+	const inactiveMembersArr = members.filter((member) => member.isActiveMember === false);
 	return inactiveMembersArr.length;
 }
 
 function populateMembersTable(active, inactive, total) {
-    document.querySelector(".active-member-count").textContent = active;
-    document.querySelector("#inactive-member-count").textContent = inactive;
-    document.querySelector("#total-member-count").textContent = total;
+	document.querySelector("#active-member-count").textContent = active;
+	document.querySelector("#inactive-member-count").textContent = inactive;
+	populateTotalNumberInTables(total);
 }
 
+function populateTotalNumberInTables(total) {
+	//Iterates all elements with class-name "total-member-count" and sets .textContent to totalMembersCount
+	const classNavn = document.querySelectorAll(".total-member-count");
+	for (const element of classNavn) {
+		element.textContent = total;
+	}
+}
 
-export { calculateMembersCount };
+export { calculateMembersCount, countInactiveMembers };
