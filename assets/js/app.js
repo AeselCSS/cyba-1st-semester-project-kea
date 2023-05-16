5c854e135ddfff529ebcb6c2c6d875f4420fa879
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-05-16 08:06:01 +0200

    grouped eventlisteners and functions by section

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ import { checkIfLoggedIn } from "./system-access.js";
 import { createMemberForm } from "./create-member.js";
 import { searchbarAndFilter } from "./search.js";
 import { sortAndShowMembers } from "./sort.js";
-import {displayMembersInDebt} from "./restance.js"
+import { displayMembersInDebt } from "./restance.js"
 import { calculateMembersCount } from "./member-table.js";
 import { displayFinancialTable } from "./member-and-finance-overview.js";
 import { refreshTop5Results } from "./results-top-five-section.js";
@@ -21,18 +21,25 @@ async function initApp() {
 	console.log(`App is running!`);
 	initViews(); // init spa router
 	checkIfLoggedIn(); // check if user is logged in
+	// members
 	await apiReadMembers();
 	//showMembers(members)
 	sortAndShowMembers(members);
+	// top 5 results
 	await apiReadResults();
 	refreshTop5Results();
+	// financial overview
+	calculateMembersCount();
+	displayFinancialTable();
+	displayMembersInDebt();
 
 	// add event listeners
-	document.querySelector("#add-new-member-btn").addEventListener("click", createMemberForm);
+	// filters on members section
+	document.querySelector("#search").addEventListener("keyup", searchbarAndFilter);
 	document.querySelector("#members-sort").addEventListener("change", () => sortAndShowMembers(members));
 	document.querySelector("#filter").addEventListener("change", searchbarAndFilter);
+	document.querySelector("#add-new-member-btn").addEventListener("click", createMemberForm);
 	document.querySelector("#checkbox-in-debt").addEventListener("change", searchbarAndFilter);
-	document.querySelector("#search").addEventListener("keyup", searchbarAndFilter);
 
 	// filters on top five section
 	document.querySelector("#age-filter").addEventListener("change", refreshTop5Results);
@@ -41,8 +48,4 @@ async function initApp() {
 		checkbox.addEventListener("change", refreshTop5Results);
 	});
 
-	//fINANCES TAB
-	calculateMembersCount();
-	displayFinancialTable();
-	displayMembersInDebt()
 }
