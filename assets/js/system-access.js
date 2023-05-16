13a72864cbab7e3870d6d3a3b1dce3bb31bd8378
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-05-16 12:18:03 +0200

    added rolespecific filters and btns

--- assets/js/system-access.js
+++ assets/js/system-access.js
@@ -1,6 +1,6 @@
 import { login } from "./login.js";
 import { logout } from "./logout.js";
-
+import { searchbarAndFilter } from "./search.js";
 
 function checkIfLoggedIn() {
   const user = localStorage.getItem("user");
@@ -15,18 +15,30 @@ function checkIfLoggedIn() {
 
 // grant access based on user role in local storage
 function systemAccess(role) {
-    // nav links and buttons
-    const membersLink = document.querySelector("#members-link")
-    const financesLink = document.querySelector("#finances-link")
-    const topFiveLink = document.querySelector("#top-five-link")
-    const loginBtn = document.querySelector("#login-btn")
-    const logoutBtn = document.querySelector("#logout-btn")
+	// nav links and buttons
+	const membersLink = document.querySelector("#members-link");
+	const financesLink = document.querySelector("#finances-link");
+	const topFiveLink = document.querySelector("#top-five-link");
+	const loginBtn = document.querySelector("#login-btn");
+	const logoutBtn = document.querySelector("#logout-btn");
 
-    // role specific buttons
-    const resultsBtn = document.querySelectorAll(".grid-item-results-btn")
-    const addMemberBtn = document.querySelector("#add-new-member-btn");
+	// search bar filters
+	const filterMale = document.querySelector("#filter-male");
+	const filterFemale = document.querySelector("#filter-female");
+	const filterNonBinary = document.querySelector("#filter-non-binary");
+	const filterActive = document.querySelector("#filter-active");
+	const filterInactive = document.querySelector("#filter-inactive");
+	const filterCompetitive = document.querySelector("#filter-competitive");
+	const filterCasual = document.querySelector("#filter-casual");
+	const filterJunior = document.querySelector("#filter-junior");
+	const filterSenior = document.querySelector("#filter-senior");
+	const competitiveCheckbox = document.querySelector("#checkbox-competitive");
+
+	// role specific buttons and checkboxes
+	const resultsBtn = document.querySelectorAll(".grid-item-results-btn");
+	const addMemberBtn = document.querySelector("#add-new-member-btn");
 	const inDebt = document.querySelector("#in-debt-container");
-
+	const competitive = document.querySelector("#competitive-filter-container");
 
 	console.log(`systemAccess: ${role} found in local storage`);
 	if (role === "chairman") {
@@ -40,6 +52,18 @@ function systemAccess(role) {
 		addMemberBtn.className = "";
 		ResultsBtnClass("hidden");
 		inDebt.className = "hidden";
+		competitive.className = "hidden";
+		// role specific filters
+		filterMale.removeAttribute("hidden");
+		filterFemale.removeAttribute("hidden");
+		filterNonBinary.removeAttribute("hidden");
+		filterActive.removeAttribute("hidden");
+		filterInactive.removeAttribute("hidden");
+		filterCompetitive.removeAttribute("hidden");
+		filterCasual.removeAttribute("hidden");
+		filterJunior.setAttribute("hidden", "");
+		filterSenior.setAttribute("hidden", "");
+		competitiveCheckbox.checked = false;
 	} else if (role === "cashier") {
 		// nav links and buttons
 		loginBtn.className = "hidden";
@@ -51,6 +75,18 @@ function systemAccess(role) {
 		addMemberBtn.className = "hidden";
 		ResultsBtnClass("hidden");
 		inDebt.className = "";
+		competitive.className = "hidden";
+		// role specific filters
+		filterMale.removeAttribute("hidden");
+		filterFemale.removeAttribute("hidden");
+		filterNonBinary.removeAttribute("hidden");
+		filterActive.removeAttribute("hidden");
+		filterInactive.removeAttribute("hidden");
+		filterCompetitive.removeAttribute("hidden");
+		filterCasual.removeAttribute("hidden");
+		filterJunior.setAttribute("hidden", "");
+		filterSenior.setAttribute("hidden", "");
+		competitiveCheckbox.checked = false;
 	} else if (role === "trainer") {
 		// nav links and buttons
 		loginBtn.className = "hidden";
@@ -62,6 +98,18 @@ function systemAccess(role) {
 		addMemberBtn.className = "hidden";
 		ResultsBtnClass("");
 		inDebt.className = "hidden";
+		competitive.className = "";
+		// role specific filters
+		filterMale.setAttribute("hidden", "");
+		filterFemale.setAttribute("hidden", "");
+		filterNonBinary.setAttribute("hidden", "");
+		filterActive.setAttribute("hidden", "");
+		filterInactive.setAttribute("hidden", "");
+		filterCompetitive.setAttribute("hidden", "");
+		filterCasual.setAttribute("hidden", "");
+		filterJunior.removeAttribute("hidden");
+		filterSenior.removeAttribute("hidden");
+		competitiveCheckbox.checked = true;
 	} else if (role === "guest") {
 		console.log("no user logged in - guest view shown");
 		// nav links and buttons
@@ -72,19 +120,24 @@ function systemAccess(role) {
 		topFiveLink.className = "hidden";
 	}
 
-    // eventlisteners
-    document.querySelector("#login-btn").addEventListener("click", login);
-    document.querySelector("#logout-btn").addEventListener("click", logout);
-    
-    // show/hide all results buttoms on member grid
-    function ResultsBtnClass (string) {
-        if (string) {
-            resultsBtn.forEach((btn)=> btn.classList.add(string))  
-        } else {
-            resultsBtn.forEach((btn) => btn.classList.remove("hidden")); 
-        }
-    }
+	// apply searchbar and filter to members section
+	searchbarAndFilter();
+
+	// eventlisteners
+	document.querySelector("#login-btn").addEventListener("click", login);
+	document.querySelector("#logout-btn").addEventListener("click", logout);
+
+	// show/hide all results buttoms on member grid
+	function ResultsBtnClass(string) {
+		if (string) {
+			resultsBtn.forEach((btn) => btn.classList.add(string));
+		} else {
+			resultsBtn.forEach((btn) => btn.classList.remove("hidden"));
+		}
+	}
 }
+
+	
 
 
 export { checkIfLoggedIn, systemAccess };
