30d50b0749a4a8b20b13b52ef63807904d6db63f
YawHB <yawhboateng@gmail.com>
2023-05-19 11:46:03 +0200

    Refactored result btn code and indebt border code

--- assets/js/show-members.js
+++ assets/js/show-members.js
@@ -38,24 +38,14 @@ function showMemberGrid(member) {
 	//Kode en if-statement, der kontrollerer at man er logget ind som cashier OG member er i gæld.
 	//Hvis ovenstående er true, så smid en class "member-in-debt" på den grid-item.
 
-	if (localStorage.getItem("user") === "cashier" && member.hasPayed === false) {
-		document.querySelector(".grid-item:last-child").classList.add("member-in-debt");
-	} else {
-		document.querySelector(".grid-item:last-child").classList.remove("member-in-debt");
-	}
-
-	addResultBtnEventlistener(member);
-
-	//hides result-button if user is not trainer
-	onlyShowResultBtnForTrainer(member);
-
-	// if (localStorage.getItem("user") !== "trainer") {
-	// 	document
-	// 		.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
-	// 		.classList.add("hidden");
-
-	// 	document.querySelector("#members-table .table-item:last-child .table-item-details-btn").classList.add("hidden");
+	// if (localStorage.getItem("user") === "cashier" && member.hasPayed === false) {
+	// 	document.querySelector(".grid-item:last-child").classList.add("member-in-debt");
+	// } else {
+	// 	document.querySelector(".grid-item:last-child").classList.remove("member-in-debt");
 	// }
+
+	showMembersIndebtForCashier("grid", member);
+	ResultBtnSetup(member);
 }
 
 function showMemberTable(member) {
@@ -82,47 +72,49 @@ function showMemberTable(member) {
 		.querySelector("#members-table .table-item:last-child .table-item-details-btn")
 		.addEventListener("click", () => memberDetailsDialog(member));
 
-	addResultBtnEventlistener(member);
-	onlyShowResultBtnForTrainer(member);
+	showMembersIndebtForCashier("table", member);
+	ResultBtnSetup(member);
 }
 
-// add click eventlistener to result button if member is competitive
-function addResultBtnEventlistener(member) {
+function ResultBtnSetup(member) {
+	// Only adds result button if member is competitive
 	if (member.isCompetitive) {
 		if (isGrid) {
-			document
-				.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
-				.addEventListener("click", () => memberResultsDialog(member));
+			eventListenerAndTrainerVisiblity("grid", member);
 		} else if (!isGrid) {
-			document
-				.querySelector("#members-table .table-item:last-child .grid-and-table-item-results-btn")
-				.addEventListener("click", () => memberResultsDialog(member));
+			eventListenerAndTrainerVisiblity("table", member);
 		}
+	}
+}
+
+function eventListenerAndTrainerVisiblity(gridOrTable, member) {
+	const resultBtn = document.querySelector(
+		`#members-${gridOrTable} .${gridOrTable}-item:last-child .grid-and-table-item-results-btn`
+	);
+
+	// Adds click event to button, opens member results dialog
+	resultBtn.addEventListener("click", () => memberResultsDialog(member));
+
+	// Hides button if not logged as trainer
+	if (localStorage.getItem("user") !== "trainer") {
+		resultBtn.classList.add("hidden");
+	}
+}
+
+function showMembersIndebtForCashier(gridOrTable, member) {
+	const person = document.querySelector(`.${gridOrTable}-item:last-child`);
+
+	// Adds red border for cashier, if member is in debt
+	if (localStorage.getItem("user") === "cashier" && member.hasPayed === false) {
+		person.classList.add("member-in-debt");
+	} else {
+		person.classList.remove("member-in-debt");
 	}
 }
 
 function clearContent() {
 	document.querySelector("#members-grid").innerHTML = "";
 	document.querySelector("#members-table-content").innerHTML = "";
-}
-
-function onlyShowResultBtnForTrainer(member) {
-	if (localStorage.getItem("user") !== "trainer") {
-		if (member.isCompetitive) {
-			if (isGrid) {
-				console.log(
-					document.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
-				);
-				document
-					.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
-					.classList.add("hidden");
-			} else if (!isGrid) {
-				document
-					.querySelector("#members-table .table-item:last-child .grid-and-table-item-results-btn")
-					.classList.add("hidden");
-			}
-		}
-	}
 }
 
 export { showMembers };
