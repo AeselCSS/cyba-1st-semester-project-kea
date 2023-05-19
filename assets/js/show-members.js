d4d821dd21740bdf75f2670de511759b7fea8e1b
YawHB <yawhboateng@gmail.com>
2023-05-19 10:36:54 +0200

    Changed structure of result button to function with both grid and table
    This solution works, but needs refactoring

--- assets/js/show-members.js
+++ assets/js/show-members.js
@@ -2,6 +2,8 @@ import { memberDetailsDialog } from "./member-detailed-view.js";
 import { memberResultsDialog } from "./member-results-view.js";
 import { calculateMemberAge } from "./member-detailed-view.js";
 
+let isGrid = true;
+
 function showMembers(members) {
 	clearContent();
 
@@ -12,6 +14,7 @@ function showMembers(members) {
 }
 
 function showMemberGrid(member) {
+	isGrid = true;
 	//Added class has-payed-true or has-payed-false. If it is has-payed-true, then a class with a red border is displayed
 	const htmlGrid = /*html*/ `
     <article class="grid-item">
@@ -41,25 +44,22 @@ function showMemberGrid(member) {
 		document.querySelector(".grid-item:last-child").classList.remove("member-in-debt");
 	}
 
-	if (member.isCompetitive) {
-		document
-			.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
-			.addEventListener("click", () => memberResultsDialog(member));
+	addResultBtnEventlistener(member);
 
-		//hides result-button if user is not trainer
-		if (localStorage.getItem("user") !== "trainer") {
-			document
-				.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
-				.classList.add("hidden");
-		}
-	}
+	//hides result-button if user is not trainer
+	onlyShowResultBtnForTrainer(member);
 
-	/* TABLE DOM kommer her og bliver outputtet i #members-table. Kommer også en eventlistener på dens knap.*/
+	// if (localStorage.getItem("user") !== "trainer") {
+	// 	document
+	// 		.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
+	// 		.classList.add("hidden");
 
-	/* TABLE DOM kommer her og bliver outputtet i #members-table. Kommer også en eventlistener på dens knap.*/
+	// 	document.querySelector("#members-table .table-item:last-child .table-item-details-btn").classList.add("hidden");
+	// }
 }
 
 function showMemberTable(member) {
+	isGrid = false;
 	const html = /*html*/ `
 		<tr class="table-item">
 		<td><img src=${member.image}></td>
@@ -72,8 +72,7 @@ function showMemberTable(member) {
 		<td id="table-flex-container-btn"> 
 			 <button class="table-item-details-btn">Details</button> 
 			${member.isCompetitive ? "<button class='grid-and-table-item-results-btn' >Results</button>" : ""} 
-</td>
-		
+		</td>
 		</tr>
 		
 		`;
@@ -83,16 +82,47 @@ function showMemberTable(member) {
 		.querySelector("#members-table .table-item:last-child .table-item-details-btn")
 		.addEventListener("click", () => memberDetailsDialog(member));
 
+	addResultBtnEventlistener(member);
+	onlyShowResultBtnForTrainer(member);
+}
+
+// add click eventlistener to result button if member is competitive
+function addResultBtnEventlistener(member) {
 	if (member.isCompetitive) {
-		document
-			.querySelector("#members-table .table-item:last-child .grid-and-table-item-results-btn")
-			.addEventListener("click", () => memberResultsDialog(member));
+		if (isGrid) {
+			document
+				.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
+				.addEventListener("click", () => memberResultsDialog(member));
+		} else if (!isGrid) {
+			document
+				.querySelector("#members-table .table-item:last-child .grid-and-table-item-results-btn")
+				.addEventListener("click", () => memberResultsDialog(member));
+		}
 	}
 }
 
 function clearContent() {
 	document.querySelector("#members-grid").innerHTML = "";
 	document.querySelector("#members-table-content").innerHTML = "";
+}
+
+function onlyShowResultBtnForTrainer(member) {
+	if (localStorage.getItem("user") !== "trainer") {
+		if (member.isCompetitive) {
+			if (isGrid) {
+				console.log(
+					document.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
+				);
+				document
+					.querySelector("#members-grid .grid-item:last-child .grid-and-table-item-results-btn")
+					.classList.add("hidden");
+			} else if (!isGrid) {
+				document
+					.querySelector("#members-table .table-item:last-child .grid-and-table-item-results-btn")
+					.classList.add("hidden");
+			}
+		}
+	}
 }
 
 export { showMembers };
