d14a3be95f5fd6150a023591366e3aad6f1ddc2d
Forkeh <forkeh@gmail.com>
2023-05-20 19:40:43 +0200

    changed dynamic background colors and made text a h3

--- assets/js/notification-feedback.js
+++ assets/js/notification-feedback.js
@@ -1,16 +1,16 @@
 function notificationFeedback(messageString, isSuccessful) {
 	const dialog = document.querySelector("#notification-feedback");
 	dialog.style.display = "flex";
-	dialog.classList.add("fade-out");
-	dialog.innerHTML = messageString;
+	dialog.classList.add("pop-in-out");
+	dialog.innerHTML = `<h3>${messageString}</h3>`;
 	dialog.addEventListener("animationend", () => {
 		dialog.style.display = "none";
 	});
 
 	if (isSuccessful) {
-		dialog.style.backgroundColor = "green";
+		dialog.style.backgroundColor = "white";
 	} else {
-		dialog.style.backgroundColor = "red";
+		dialog.style.backgroundColor = "rgb(249, 95, 95)";
 	}
 }
 
