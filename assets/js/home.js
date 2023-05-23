6410de7b54a67baba17beb9c353f4f824cb7ab60
AliHMohammad <alihmohammad@hotmail.com>
2023-05-23 15:00:19 +0200

    Added min max to birthdate and added notification on send button

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,10 @@
-
+import { createMinMaxDate } from "./helpers-module.js"
+import {notificationFeedback} from "./notification-feedback.js"
 
 function displayJoinForm() {
     document.querySelector("#main-dialog").innerHTML = "";
     
-    const html = /*html*/`
+    const html = /*html*/ `
 		<form id="join-form">
 			<div id="form-content">
 				<label for="firstName">First name</label>
@@ -16,7 +17,9 @@ function displayJoinForm() {
 				<input type="email" name="email" id="email" required />
 
 				<label for="dateOfBirth">Date of birth</label>
-				<input type="date" name="dateOfBirth" id="dateOfBirth" required />
+				<input type="date" name="dateOfBirth" id="dateOfBirth" min=${createMinMaxDate(100)} max=${createMinMaxDate(
+		5
+	)} required />
 
 				<label for="gender">Gender</label>
 				<select name="gender" id="gender" required>
@@ -30,7 +33,7 @@ function displayJoinForm() {
 					<input id="home-form-input-button" type="button" value="Send"/>
 				</div>
 		</form>
-    `
+    `;
 
     document.querySelector("#main-dialog").insertAdjacentHTML("beforeend", html);
     document.querySelector("#main-dialog-frame").showModal();
@@ -40,7 +43,7 @@ function displayJoinForm() {
 
 function sendForm() {
     document.querySelector("#main-dialog-frame").close();
-    //TODO : Notifikation popper op og tilkendegiver, at formen er sendt
+	notificationFeedback("Success! We will reach back to you through Email", true);
 }
 
 export {displayJoinForm}
