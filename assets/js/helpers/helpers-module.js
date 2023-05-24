9ead19e869ffb1ee83e10b79314b60ceccc02bdd
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-05-24 21:36:56 +0200

    added initiateEventlisteners and imports
    Co-authored-by: Ali Mohammad <AliHMohammad@users.noreply.github.com>

--- assets/js/helpers/helpers-module.js
+++ assets/js/helpers/helpers-module.js
@@ -1,3 +1,9 @@
+import {displayJoinForm} from "../home/home.js";
+import { refreshFiltersAndSort } from "../members/filter-and-sort.js";
+import { createMemberForm } from "../members/create-member.js";
+import { toggleTableGridView } from "./toggle-table-grid.js";
+import { refreshTop5Results } from "../results/results-top-five-section.js";
+
 function closeDialogEventListener() {
 	const closeDialogButton = document.querySelector("#btn-close-dialog-modal");
 	const dialogContent = document.querySelector("#main-dialog");
@@ -31,4 +37,25 @@ function calculateMemberAge(member) {
 	return age;
 }
 
-export { closeDialogEventListener, resetFilterSearchSort, createMinMaxDate, calculateMemberAge };
+function initiateEventlisteners() {
+	// add event listeners
+	document.querySelector("#join-btn").addEventListener("click", displayJoinForm);
+	// filters on members section
+	closeDialogEventListener();
+	document.querySelector("#search").addEventListener("input", refreshFiltersAndSort);
+	document.querySelector("#members-sort").addEventListener("change", refreshFiltersAndSort);
+	document.querySelector("#filter").addEventListener("change", refreshFiltersAndSort);
+	document.querySelector("#add-new-member-btn").addEventListener("click", createMemberForm);
+	document.querySelector("#checkbox-in-debt").addEventListener("change", refreshFiltersAndSort);
+	document.querySelector("#checkbox-competitive").addEventListener("change", refreshFiltersAndSort);
+	document.querySelector("#toggle-table-grid").addEventListener("click", toggleTableGridView);
+
+	// filters on top five section
+	document.querySelector("#age-filter").addEventListener("change", refreshTop5Results);
+	document.querySelector("#gender-filter").addEventListener("change", refreshTop5Results);
+	document.querySelectorAll(".result-type-filter").forEach((checkbox) => {
+		checkbox.addEventListener("change", refreshTop5Results);
+	});
+}
+
+export { closeDialogEventListener, resetFilterSearchSort, createMinMaxDate, calculateMemberAge, initiateEventlisteners };
